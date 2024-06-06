0b25966efe2d611ac02a2eec9b2acbc4cf5af1fe
ANU-PRASAD-S <143721871+ANU-PRASAD-S@users.noreply.github.com>
2024-06-06 16:43:51 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -16,7 +16,7 @@ st.title("YouTube Channel Data Retriever")
 
 # Section for entering API token
 st.header("Enter API Token")
-api_token = st.text_input("API Token", type="password")
+api_token = st.text_input("API Token")
 api_key = "api_token"
 
 # Section for entering YouTube channel ID
